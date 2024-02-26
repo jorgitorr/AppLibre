10cba65cbf01e61da042204a4ea3a6c38dc34cab
jorge <jorgearceruiz97@gmail.com>
2024-02-26 19:53:33 +0100

    añadido icono app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    testImplementation ("io.mockk:mockk:1.12.0") //implementacion de mock para pruebas unitarias
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
