1f459ab8a6e5f65143f4dc2365e640ec43dbb91f
jorge <jorgearceruiz97@gmail.com>
2024-02-29 20:59:57 +0100

    añadidos colores y más cosas como texto en heriodetailfav

--- app/src/main/java/com/example/applibre/ui/view/principal/HeroDetail.kt
+++ app/src/main/java/com/example/applibre/ui/view/principal/HeroDetail.kt
@@ -38,6 +38,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -127,6 +128,10 @@ fun HeroDetailView(heroDeckViewModel: HeroDeckViewModel, navController: NavContr
                 }
                 Skills(superHero = superHero)
                 Spacer(modifier = Modifier.padding(15.dp))
+
+
+
+
 
                 Text(text = "GUARDAR",
                     Modifier
@@ -221,6 +226,9 @@ fun Skills(superHero:SuperHero){
         }
     }
 }
+
+
+
 
 
 
