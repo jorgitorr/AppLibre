bb06b6ca7504975d6303a01c4ec770533f18f16b
jorge <jorgearceruiz97@gmail.com>
2024-02-29 17:58:17 +0100

    intentando poner el search button

--- app/src/main/java/com/example/applibre/ui/model/HeroDeckViewModel.kt
+++ app/src/main/java/com/example/applibre/ui/model/HeroDeckViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.toLowerCase
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.applibre.data.model.SuperHero
@@ -21,7 +20,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import java.io.IOException
 import java.lang.Exception
-import kotlin.random.Random
 
 /**
  * lógica de las cartas y de los SuperHeroes
--- app/src/main/java/com/example/applibre/ui/view/components/Filtrar.kt
+++ app/src/main/java/com/example/applibre/ui/view/components/Filtrar.kt
@@ -1,21 +1,15 @@
 package com.example.applibre.ui.view.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
